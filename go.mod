c149746374a2a880e472cc7b2977fab6b33c99c2
shnartho <shnartho@gmail.com>
2023-08-15 17:26:02 +0100

    env added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shnartho/Backend-Web-Server-using-Go-Postgres
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	fmt.Println("Port:", portString)
 }
